07c7124f9c4cb9ad4a76671859d56c932680c8f2
skushnirenko-fe <skuhnirenko@forgottenempires.net>
2025-09-18 22:23:25 +0200

    invasions: loading invasion points from config also

--- src/scenario/editor.cpp
+++ src/scenario/editor.cpp
@@ -67,8 +67,10 @@ void scenario_editor_create(int map_size) {
     g_scenario.exit_point.set(-1);
     g_scenario.river_entry_point.set(-1);
     g_scenario.river_exit_point.set(-1);
-    for (int i = 0; i < MAX_INVASION_POINTS_LAND; i++)
-        g_scenario.invasion_points_land[i].set(-1);
+
+    auto& inv_landp = g_scenario.invasion_points_land;
+    std::fill(inv_landp.begin(), inv_landp.end(), tile2i::invalid);
+
     for (int i = 0; i < MAX_INVASION_POINTS_SEA; i++)
         g_scenario.invasion_points_sea[i].set(-1);
 
