18c651532acd2d4e1fd3f752e846b260c0397956
dalerank <dalerankn8@gmail.com>
2025-09-21 20:22:22 +0200

    refactor: loading mission_config as usual scheme from js

--- src/sound/sound_mission.h
+++ src/sound/sound_mission.h
@@ -1,12 +1,26 @@
 #pragma once
 
-#include "core/xstring.h"
+#include "core/archive.h"
+#include <type_traits>
 
 namespace snd {
     struct mission_config {
+        int mission;
         xstring briefing;
         xstring won;
+        bool operator==(const mission_config &other) const noexcept { return mission == other.mission; }
+        bool operator!=(const mission_config &other) const noexcept { return mission != other.mission; }
+        bool operator<(const mission_config &other) const noexcept { return mission < other.mission; }
     };
 
     mission_config get_mission_config(int mission);
 }
+
+template<>
+struct std::hash<snd::mission_config> {
+    std::size_t operator()(const snd::mission_config &k) const noexcept {
+        return k.mission;
+    }
+};
+
+ANK_CONFIG_STRUCT(snd::mission_config, mission, briefing, won)
