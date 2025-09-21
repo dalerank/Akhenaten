ad5f40a6e5a0e932332151273fce35f3c5852880
dalerank <dalerankn8@gmail.com>
2025-09-21 02:06:44 +0200

    fixed compilation on clang

--- src/core/archive.h
+++ src/core/archive.h
@@ -528,7 +528,7 @@ struct g_archive_section {
 #define ANK_CONFIG_STRUCT_PASTE63(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37, v38, v39, v40, v41, v42, v43, v44, v45, v46, v47, v48, v49, v50, v51, v52, v53, v54, v55, v56, v57, v58, v59, v60, v61, v62) ANK_CONFIG_STRUCT_PASTE2(func, v1) ANK_CONFIG_STRUCT_PASTE62(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37, v38, v39, v40, v41, v42, v43, v44, v45, v46, v47, v48, v49, v50, v51, v52, v53, v54, v55, v56, v57, v58, v59, v60, v61, v62)
 #define ANK_CONFIG_STRUCT_PASTE64(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37, v38, v39, v40, v41, v42, v43, v44, v45, v46, v47, v48, v49, v50, v51, v52, v53, v54, v55, v56, v57, v58, v59, v60, v61, v62, v63) ANK_CONFIG_STRUCT_PASTE2(func, v1) ANK_CONFIG_STRUCT_PASTE63(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37, v38, v39, v40, v41, v42, v43, v44, v45, v46, v47, v48, v49, v50, v51, v52, v53, v54, v55, v56, v57, v58, v59, v60, v61, v62, v63)
 
-#define ANK_CONFIG_STRUCT_FROM(v1) js_j.r<decltype(js_t.v1)>(#v1, js_t.v1); 
+#define ANK_CONFIG_STRUCT_FROM(v1) js_j.r(#v1, js_t.v1); 
 
 template<typename, typename = void>
 struct class_has_load_function : std::false_type {};
