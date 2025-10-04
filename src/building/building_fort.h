a68b5cad0d7ce75bf0b6805bdd64066107730b41
dalerank <dalerankn8@gmail.com>
2025-10-04 13:19:11 +0200

    buildings: fort config use deserialization for loading ghost config

--- src/building/building_fort.h
+++ src/building/building_fort.h
@@ -3,6 +3,13 @@
 #include "building/building.h"
 #include "window/window_building_info.h"
 
+struct building_fort_ghost {
+    svector<vec2i, 4> main_view_offset;
+    svector<vec2i, 4> ground_view_offset;
+    svector<vec2i, 32> ground_check_offset;
+};
+ANK_CONFIG_STRUCT(building_fort_ghost, main_view_offset, ground_view_offset, ground_check_offset)
+
 class building_fort : public building_impl {
 public:
     building_fort(building &b) : building_impl(b) {}
@@ -10,18 +17,10 @@ public:
 
     template<typename T>
     struct static_params_t : buildings::model_t<T> {
-        struct {
-            svector<vec2i, 4> main_view_offset;
-            svector<vec2i, 4> ground_view_offset;
-            svector<vec2i, 32> ground_check_offset;
-        } ghost;
+        building_fort_ghost ghost;
 
         virtual void archive_load(archive arch) override {
-            arch.r_section("ghost", [this] (archive ghost_arch) {
-                ghost.main_view_offset = ghost_arch.r_array_vec2i("main");
-                ghost.ground_view_offset = ghost_arch.r_array_vec2i("ground");
-                ghost.ground_check_offset = ghost_arch.r_array_vec2i("ground_check");
-            });
+            arch.r("ghost", ghost);
         }
 
         virtual void planer_ghost_preview(build_planner &planer, painter &ctx, tile2i tile, tile2i end, vec2i pixel) const override;
