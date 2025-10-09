8b845cdaee5a749b4fc8b132d0c1f985c64f35d6
dalerank <dalerankn8@gmail.com>
2025-10-09 23:18:24 +0200

    figures: add persian enemies archer/spearman

--- src/figuretype/figure_enemy_archer.cpp
+++ src/figuretype/figure_enemy_archer.cpp
@@ -16,6 +16,7 @@ figure_hittite_archer::static_params hittite_archer_m;
 figure_hyksos_archer::static_params hyksos_archer_m;
 figure_libian_archer::static_params libian_archer_m;
 figure_nubian_archer::static_params nubian_archer_m;
+figure_persian_archer::static_params persian_archer_m;
 
 void figure_enemy_archer::on_create() {
     figure_impl::on_create();
