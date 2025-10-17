33551c4132771893310aa2522130b29cc45320e1
MacThings <98193439+MacThings@users.noreply.github.com>
2025-10-17 16:30:12 +0200

    Enhance build_macos.sh for SDL2 integration
    Updated build script to include SDL2 and SDL2_mixer paths.

--- build_macos.sh
+++ build_macos.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-cmake --build ./build --target clean
-cmake -B build -DCMAKE_C_FLAGS="-UTARGET_OS_MAC -U__MWERKS__ -Uapplec -UTHINK_C -U__SC__"
-cmake --build ./build
+CORES=$(sysctl -n hw.ncpu)
+
+cmake --build ./build --target clean -- -j"$CORES"
+
+cmake -B build \
+  -DCMAKE_C_FLAGS="-I/opt/homebrew/include/SDL2 -UTARGET_OS_MAC -U__MWERKS__ -Uapplec -UTHINK_C -U__SC__" \
+  -DCMAKE_CXX_FLAGS="-I/opt/homebrew/include/SDL2" \
+  -DCMAKE_EXE_LINKER_FLAGS="-L/opt/homebrew/lib" \
+  -DCMAKE_PREFIX_PATH="/opt/homebrew"
+
+cmake --build ./build -- -j"$CORES"
 
 cd build/akhenaten.app/Contents/MacOS
 
