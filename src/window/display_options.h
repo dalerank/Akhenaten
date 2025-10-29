651db1c54c58fb0603a58e30d23e727cdbea570c
dalerank <dalerankn8@gmail.com>
2025-10-29 20:57:52 +0100

    ui: fixed display_options window mouse handling

--- src/window/display_options.h
+++ src/window/display_options.h
@@ -15,7 +15,8 @@ namespace ui {
         virtual int draw_background(UiFlags flags) override { return 0; }
         virtual void ui_draw_foreground(UiFlags flags) override;
         virtual int ui_handle_mouse(const mouse *m) override;
-        virtual void init() override {}
+        virtual void init() override {};
+        virtual void archive_load(archive arch) override;
 
         void init(close_callback close_cb);
 
@@ -28,6 +29,7 @@ namespace ui {
         vec2i original_resolution;
         vec2i selected_resolution;
         scroll_list_panel* panel = nullptr;
+        int num_files_in_view;
         std::vector<video_mode> video_modes;
     };
 }
