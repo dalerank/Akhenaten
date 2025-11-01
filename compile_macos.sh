4402a9d57a667d89158176465fffc07cd8a188c6
MacThings <98193439+MacThings@users.noreply.github.com>
2025-11-01 12:08:01 +0100

    Update compile_macos.sh
    Added Universal Binary Option

--- compile_macos.sh
+++ compile_macos.sh
@@ -1,29 +1,72 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]; then
-  echo "Usage: ./compile_macos [arm64 | x86_64]"
-  exit 1
-fi
-
-if [ "$1" != "arm64" ] && [ "$1" != "x86_64" ]; then
-  echo "Error: Invalid architecture '$1'"
-  echo "Usage: ./compile_macos [arm64 | x86_64]"
-  exit 1
-fi
+MAIN_DIR="$PWD"
 
 CORES=$(sysctl -n hw.ncpu)
 
-cmake --build ./build --target clean -- -j"$CORES"
-cmake -B build -DCMAKE_OSX_ARCHITECTURES="$1" -DCMAKE_C_FLAGS="-UTARGET_OS_MAC -U__MWERKS__ -Uapplec -UTHINK_C -U__SC__"
-cmake --build ./build -- -j"$CORES"
+if [ -z "$1" ]; then
+  echo "Usage: ./compile_macos [arm64|x86_64|u2b]"
+  exit 1
+fi
 
-cd build/akhenaten.app/Contents/MacOS
-mkdir ../Frameworks Data 2>/dev/null
+if [ "$1" != "arm64" ] && [ "$1" != "x86_64" ] && [ "$1" != "u2b" ]; then
+  echo "Error: Invalid option '$1'"
+  echo "Usage: ./compile_macos [arm64|x86_64|u2b]"
+  exit 1
+fi
 
-cp -R "$HOME"/Library/Frameworks/SDL2.framework ../Frameworks/
-cp -R "$HOME"/Library/Frameworks/SDL2_mixer.framework ../Frameworks/
-cp ../../../../data/* Data/
-cp ../../../../res/macos/launch.sh .
-chmod +x launch.sh
+function assemble_package()
+{
+	cd build/akhenaten.app/Contents/MacOS
+	mkdir ../Frameworks Data 2>/dev/null
+	
+	cp -R "$HOME"/Library/Frameworks/SDL2.framework ../Frameworks/
+	cp -R "$HOME"/Library/Frameworks/SDL2_mixer.framework ../Frameworks/
+	cp ../../../../data/* Data/
+	cp ../../../../res/macos/launch.sh .
+	chmod +x launch.sh
+}
 
-install_name_tool -add_rpath "@executable_path/../Frameworks" akhenaten
+function compile_arm64()
+{
+	rm -rf build/akhenaten.app 2>/dev/null
+	cmake --build ./build --target clean -- -j"$CORES"
+	cmake -B build -DCMAKE_OSX_ARCHITECTURES="arm64" -DCMAKE_C_FLAGS="-UTARGET_OS_MAC -U__MWERKS__ -Uapplec -UTHINK_C -U__SC__"
+	cmake --build ./build -- -j"$CORES"
+	assemble_package	
+}
+
+function compile_x86_64()
+{
+	rm -rf build/akhenaten.app 2>/dev/null
+	cmake --build ./build --target clean -- -j"$CORES"
+	cmake -B build -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_C_FLAGS="-UTARGET_OS_MAC -U__MWERKS__ -Uapplec -UTHINK_C -U__SC__"
+	cmake --build ./build -- -j"$CORES"
+	assemble_package
+}
+
+function compile_u2b()
+{
+	rm -f /private/tmp/akhenaten_* 2>/dev/null
+	compile_x86_64
+	install_name_tool -add_rpath "@executable_path/../Frameworks" akhenaten
+	mv akhenaten /private/tmp/akhenaten_x86
+	cd "$MAIN_DIR"
+	compile_arm64
+	install_name_tool -add_rpath "@executable_path/../Frameworks" akhenaten
+	mv akhenaten /private/tmp/akhenaten_arm
+	lipo /private/tmp/akhenaten_arm /private/tmp/akhenaten_x86 -output akhenaten -create
+}
+
+if [ "$1" = "arm64" ]; then
+	compile_arm64
+fi
+
+if [ "$1" = "x86_64" ]; then
+	compile_x86_64
+fi
+
+if [ "$1" = "u2b" ]; then
+	compile_u2b
+fi
+
