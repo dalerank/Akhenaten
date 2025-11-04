5eaddf019905c0e06492e30fc58eeadfe73a6f28
dalerank <dalerankn8@gmail.com>
2025-11-04 18:25:51 +0100

    js: add property modifiers for simple set/get initialization

--- ext/mujs/jscompile.c
+++ ext/mujs/jscompile.c
@@ -317,6 +317,34 @@ static void checkdup(JF, js_Ast *list, js_Ast *end)
 	}
 }
 
+/* Helper to check if a modifier exists */
+static int has_modifier(js_AstModifier *modifiers, const char *key)
+{
+	while (modifiers) {
+		if (strcmp(modifiers->key, key) == 0)
+			return 1;
+		modifiers = modifiers->next;
+	}
+	return 0;
+}
+
+/* Helper to find a property in an object literal by name */
+static js_Ast *find_object_property(js_Ast *objlist, const char *name)
+{
+	while (objlist) {
+		js_Ast *prop = objlist->a;
+		if (prop && prop->a) {
+			js_Ast *propname = prop->a;
+			if ((propname->type == AST_IDENTIFIER || propname->type == EXP_STRING) &&
+				strcmp(propname->string, name) == 0) {
+				return prop;
+			}
+		}
+		objlist = objlist->b;
+	}
+	return NULL;
+}
+
 static void cobject(JF, js_Ast *list)
 {
 	js_Ast *head = list;
@@ -325,30 +353,104 @@ static void cobject(JF, js_Ast *list)
 		js_Ast *kv = list->a;
 		js_Ast *prop = kv->a;
 
-		if (prop->type == AST_IDENTIFIER || prop->type == EXP_STRING)
-			emitstring(J, F, OP_STRING, prop->string);
-		else if (prop->type == EXP_NUMBER)
-			emitnumber(J, F, prop->number);
-		else
-			jsC_error(J, prop, "invalid property name in object initializer");
+		/* Check if this property has [property] modifier - handle it separately */
+		if (kv->type == EXP_PROP_VAL && kv->modifiers && has_modifier(kv->modifiers, "property")) {
+			/* Check if value is an object literal with get/set */
+			if (kv->b && kv->b->type == EXP_OBJECT && kv->b->a) {
+				js_Ast *getter_prop = find_object_property(kv->b->a, "get");
+				js_Ast *setter_prop = find_object_property(kv->b->a, "set");
+				
+				/* Emit getter if present */
+				if (getter_prop && getter_prop->b) {
+					/* Emit property name for getter */
+					if (prop->type == AST_IDENTIFIER || prop->type == EXP_STRING)
+						emitstring(J, F, OP_STRING, prop->string);
+					else if (prop->type == EXP_NUMBER)
+						emitnumber(J, F, prop->number);
+					else
+						jsC_error(J, prop, "invalid property name in object initializer");
+					
+					/* getter_prop->b should be the function */
+					if (getter_prop->b->type == EXP_FUN) {
+						emitfunction(J, F, newfun(J, getter_prop->b, NULL, getter_prop->b->b, getter_prop->b->c, 0));
+					} else {
+						/* If it's not a function expression, compile it as expression */
+						cexp(J, F, getter_prop->b);
+					}
+					emit(J, F, OP_INITGETTER);
+				}
+				
+				/* Emit setter if present */
+				if (setter_prop && setter_prop->b) {
+					/* Emit property name for setter */
+					if (prop->type == AST_IDENTIFIER || prop->type == EXP_STRING)
+						emitstring(J, F, OP_STRING, prop->string);
+					else if (prop->type == EXP_NUMBER)
+						emitnumber(J, F, prop->number);
+					else
+						jsC_error(J, prop, "invalid property name in object initializer");
+					
+					/* setter_prop->b should be the function */
+					if (setter_prop->b->type == EXP_FUN) {
+						emitfunction(J, F, newfun(J, setter_prop->b, NULL, setter_prop->b->b, setter_prop->b->c, 0));
+					} else {
+						/* If it's not a function expression, compile it as expression */
+						cexp(J, F, setter_prop->b);
+					}
+					emit(J, F, OP_INITSETTER);
+				}
+				
+				/* If neither getter nor setter found, treat as normal property */
+				if (!getter_prop && !setter_prop) {
+					if (prop->type == AST_IDENTIFIER || prop->type == EXP_STRING)
+						emitstring(J, F, OP_STRING, prop->string);
+					else if (prop->type == EXP_NUMBER)
+						emitnumber(J, F, prop->number);
+					else
+						jsC_error(J, prop, "invalid property name in object initializer");
+					
+					cexp(J, F, kv->b);
+					emit(J, F, OP_INITPROP);
+				}
+			} else {
+				/* Not an object literal, treat as normal property */
+				if (prop->type == AST_IDENTIFIER || prop->type == EXP_STRING)
+					emitstring(J, F, OP_STRING, prop->string);
+				else if (prop->type == EXP_NUMBER)
+					emitnumber(J, F, prop->number);
+				else
+					jsC_error(J, prop, "invalid property name in object initializer");
+				
+				cexp(J, F, kv->b);
+				emit(J, F, OP_INITPROP);
+			}
+		} else {
+			/* Normal property handling */
+			if (prop->type == AST_IDENTIFIER || prop->type == EXP_STRING)
+				emitstring(J, F, OP_STRING, prop->string);
+			else if (prop->type == EXP_NUMBER)
+				emitnumber(J, F, prop->number);
+			else
+				jsC_error(J, prop, "invalid property name in object initializer");
 
-		if (J->strict)
-			checkdup(J, F, head, kv);
+			if (J->strict)
+				checkdup(J, F, head, kv);
 
-		switch (kv->type) {
-		default: /* impossible */ break;
-		case EXP_PROP_VAL:
-			cexp(J, F, kv->b);
-			emit(J, F, OP_INITPROP);
-			break;
-		case EXP_PROP_GET:
-			emitfunction(J, F, newfun(J, kv, NULL, kv->b, kv->c, 0));
-			emit(J, F, OP_INITGETTER);
-			break;
-		case EXP_PROP_SET:
-			emitfunction(J, F, newfun(J, kv, NULL, kv->b, kv->c, 0));
-			emit(J, F, OP_INITSETTER);
-			break;
+			switch (kv->type) {
+			default: /* impossible */ break;
+			case EXP_PROP_VAL:
+				cexp(J, F, kv->b);
+				emit(J, F, OP_INITPROP);
+				break;
+			case EXP_PROP_GET:
+				emitfunction(J, F, newfun(J, kv, NULL, kv->b, kv->c, 0));
+				emit(J, F, OP_INITGETTER);
+				break;
+			case EXP_PROP_SET:
+				emitfunction(J, F, newfun(J, kv, NULL, kv->b, kv->c, 0));
+				emit(J, F, OP_INITSETTER);
+				break;
+			}
 		}
 
 		list = list->b;
