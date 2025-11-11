1461f00d7fc7ab3894f404ade418ab98021a8fdf
dalerank <dalerankn8@gmail.com>
2025-11-11 20:59:20 +0100

    figures: improved sounds for scriber

--- src/figuretype/figure_scriber.h
+++ src/figuretype/figure_scriber.h
@@ -9,8 +9,7 @@ public:
 
     virtual void on_create() override {}
     virtual void figure_action() override;
-    virtual figure_phrase_t phrase() const override { return {FIGURE_SCRIBER, "scribe"}; }
-    //virtual sound_key phrase_key() const override;
+    virtual sound_key phrase_key() const override;
     virtual int provide_service() override;
     virtual e_overlay get_overlay() const override { return OVERLAY_SCRIBAL_SCHOOL; }
     //virtual figure_sound_t get_sound_reaction(pcstr key) const override;
