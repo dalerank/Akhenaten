1cf81191cfa9cc86dad198c1a841a12f4c57676e
dalerank <dalerankn8@gmail.com>
2025-11-15 01:28:20 +0100

    scripts: add function __city_building_is_tax_collector

--- src/city/city_buildings_js.cpp
+++ src/city/city_buildings_js.cpp
@@ -16,3 +16,9 @@ void __city_set_advisor_available(int advisor, int available) {
     g_city.set_advisor_available((e_advisor)advisor, (e_availability)available);
 }
 ANK_FUNCTION_2(__city_set_advisor_available)
+
+bool __city_building_is_tax_collector(int bid) {
+    building *b = building_get(bid);
+    return !!b->dcast_tax_collector();
+}
+ANK_FUNCTION_1(__city_building_is_tax_collector)
