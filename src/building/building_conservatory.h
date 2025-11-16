11f38138f03f411cf1713efede90a65a0204226d
dalerank <dalerankn8@gmail.com>
2025-11-17 00:49:59 +0100

    buildings: improved logic for conservatory

--- src/building/building_conservatory.h
+++ src/building/building_conservatory.h
@@ -8,9 +8,14 @@ public:
 
     virtual building_conservatory *dcast_conservatory() override { return this; }
 
+    struct static_params : public building_static_params {
+        uint8_t spawn_interval;
+    } BUILDING_STATIC_DATA_T;
+
     virtual void spawn_figure() override;
-    virtual e_overlay get_overlay() const override { return OVERLAY_ENTERTAINMENT; }
+    virtual void update_day() override;
     virtual void update_graphic() override;
     virtual e_sound_channel_city sound_channel() const override { return SOUND_CHANNEL_CITY_CONSERVATORY; }
     virtual bool draw_ornaments_and_animations_height(painter &ctx, vec2i point, tile2i tile, color mask) override;
 };
+ANK_CONFIG_STRUCT(building_conservatory::static_params, spawn_interval)
