035075e91100fe98bd08f252c7c4bcbb39ece8dd
dalerank <dalerankn8@gmail.com>
2025-11-16 21:27:05 +0100

    ui: removed obsolete mouse handling for bazaar info window

--- src/building/building_bazaar.h
+++ src/building/building_bazaar.h
@@ -6,8 +6,12 @@
 class building_bazaar : public building_impl {
 public:
     BUILDING_METAINFO(BUILDING_BAZAAR, building_bazaar, building_impl)
-
     virtual building_bazaar *dcast_bazaar() override { return this; }
+
+    struct static_params : public building_static_params {
+        uint8_t max_search_distance;
+        uint8_t fancy_treshold_desirability;
+    } BUILDING_STATIC_DATA_T;
 
     struct runtime_data_t {
         uint16_t inventory[8];
@@ -22,15 +26,11 @@ public:
     virtual void on_create(int orientation) override;
     virtual void spawn_figure() override;
     virtual void update_graphic() override;
-    virtual int window_info_handle_mouse(const mouse *m, object_info &c) override;
-    virtual e_overlay get_overlay() const override { return OVERLAY_BAZAAR_ACCESS; }
     virtual e_sound_channel_city sound_channel() const override { return SOUND_CHANNEL_CITY_MARKET; }
     virtual bool draw_ornaments_and_animations_height(painter &ctx, vec2i point, tile2i tile, color color_mask) override;
     virtual void bind_dynamic(io_buffer *iob, size_t version) override;
 
     building *get_storage_destination();
-    int handle_mouse_orders(const mouse *m, object_info &c);
-    int handle_mouse_simple(const mouse *m, object_info &c);
     int max_food_stock();
     int max_goods_stock();
     bool is_good_accepted(int index);
@@ -39,4 +39,5 @@ public:
     inline int allow_food_types() const { return 4; }
     inline int allow_good_types() const { return 4; }
 };
+ANK_CONFIG_STRUCT(building_bazaar::static_params, max_search_distance, fancy_treshold_desirability)
 
