b4a82b4a8774477399a3db196cff0631883ece24
dalerank <dalerankn8@gmail.com>
2025-11-17 00:34:49 +0100

    Fix cattle ranch to match original Pharaoh 1999 behavior
    - Add straw availability check in can_play_animation()
    - Add visual representation of stored straw

--- src/building/building_cattle_ranch.cpp
+++ src/building/building_cattle_ranch.cpp
@@ -2,12 +2,18 @@
 
 #include "building/building_workshop.h"
 #include "graphics/animation.h"
+#include "graphics/graphics.h"
+#include "graphics/image.h"
 #include "widget/city/ornaments.h"
 #include "js/js_game.h"
 
 REPLICATE_STATIC_PARAMS_FROM_CONFIG(building_cattle_ranch);
 
 bool building_cattle_ranch::can_play_animation() const {
+    if (base.stored_amount(RESOURCE_STRAW) < 100) {
+        return false;
+    }
+
     if (worker_percentage() < 50) {
         return false;
     }
@@ -17,6 +23,20 @@ bool building_cattle_ranch::can_play_animation() const {
 
 bool building_cattle_ranch::draw_ornaments_and_animations_height(painter &ctx, vec2i point, tile2i tile, color color_mask) {
     draw_normal_anim(ctx, point, tile, color_mask);
+
+    int amount = ceilf(base.stored_amount(RESOURCE_STRAW) / 100.f);
+    if (amount > 0) {
+        const auto &ranim = anim(animkeys().straw);
+        vec2i pos = ranim.pos;
+        for (int i = 0; i < amount; ++i) {
+            auto& command = ImageDraw::create_subcommand(render_command_t::ert_generic);
+            command.image_id = ranim.first_img();
+            command.pixel = point + pos;
+            command.mask = color_mask;
+
+            pos += {5, -5};
+        }
+    }
 
     return true;
 }
