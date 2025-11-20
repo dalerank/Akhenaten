f3db8f73c70ec4255975fa179e82a94c1794a1c9
dalerank <dalerankn8@gmail.com>
2025-11-21 00:57:15 +0100

    solution: bzip moved to src folder from ext

--- ext/aes/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := aes
-LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,, \
-	$(wildcard $(LOCAL_PATH)/*.cpp))
-
-include $(BUILD_SHARED_LIBRARY)
