b757feb9a1984a599a60792863abaad4a6accbdd
skushnirenko-fe <skuhnirenko@forgottenempires.net>
2025-11-24 09:55:26 +0100

    solution: removed sdl lobs from update scripts

--- update-workspace-bazzite.sh
+++ update-workspace-bazzite.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-brew install sdl2 sdl2_mixer sdl2_image
+brew install
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
--- update-workspace-linux.sh
+++ update-workspace-linux.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo apt -qq update
 # Note: libpng-dev is not needed as libpng is built from source via FetchContent
-# Installing it could cause conflicts with the bundled zlib
-sudo apt install --yes git cmake build-essential libsdl2-dev libsdl2-mixer-dev libsdl2-image-dev
+# Installing it could cause conflicts with the bundled 
+sudo apt install --yes git cmake build-essential
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
