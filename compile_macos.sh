ed6097e4767592a3b00d8095600d4e08fefdd4ca
MacThings <98193439+MacThings@users.noreply.github.com>
2025-11-26 16:05:17 +0100

    Update compile_macos.sh

--- compile_macos.sh
+++ compile_macos.sh
@@ -8,12 +8,5 @@ rm -rf build 2>/dev/null
 mkdir build
 
 cmake --build ./build --target clean -- -j"$CORES"
-cmake -B build -DCMAKE_C_FLAGS="-UTARGET_OS_MAC -U__MWERKS__ -Uapplec -UTHINK_C -U__SC__"
+cmake -B build -DPNG_LIBRARY="/opt/homebrew/opt/libpng/lib"
 cmake --build ./build -- -j"$CORES"
-
-cd build/akhenaten.app/Contents/MacOS
-mkdir Data 2>/dev/null
-
-cp ../../../../data/* Data/
-cp ../../../../res/macos/launch.sh .
-chmod +x launch.sh	
