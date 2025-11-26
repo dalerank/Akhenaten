284fb6509377134f64adf4a7fc36f6901e861f5a
MacThings <98193439+MacThings@users.noreply.github.com>
2025-11-26 11:43:39 +0100

    Seperate x86 support

---
+++ compile_macos_x86.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+MAIN_DIR="$PWD"
+
+CORES=$(sysctl -n hw.ncpu)
+
+rm -rf build 2>/dev/null
+mkdir build
+
+function assemble_package()
+{
+	cd build/akhenaten.app/Contents/MacOS
+	mkdir ../Frameworks Data 2>/dev/null
+	
+	cp ../../../../data/* Data/
+	cp ../../../../res/macos/launch.sh .
+	chmod +x launch.sh
+}
+
+function compile_x86_64()
+{
+	export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
+	export LDFLAGS="-L/usr/local/lib"
+	export CFLAGS="-I/usr/local/include"
+	export CPPFLAGS="-I/usr/local/include"
+	
+	export PKG_CONFIG_PATH="/opt/homebrew/lib/pkgconfig"
+  cmake -DZLIB_LIBRARY="/opt/homebrew/lib/libz.dylib" -DZLIB_INCLUDE_DIR="/opt/homebrew/include" ..
+
+	cmake --build ./build --target clean -- -j"$CORES"
+	cmake -B build -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_C_FLAGS="-UTARGET_OS_MAC -U__MWERKS__ -Uapplec -UTHINK_C -U__SC__"
+	cmake --build ./build -- -j"$CORES"
+	assemble_package
+}
+compile_x86_64
+
+
