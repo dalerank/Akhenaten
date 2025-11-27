8348f5b7cbd014e08e3032dc5ab992bf7db408e0
MacThings <info@sl-soft.de>
2025-11-27 18:36:22 +0100

    Minor fix

--- compile_macos.sh
+++ compile_macos.sh
@@ -9,9 +9,6 @@ cmake --build ./build --target clean -- -j"$CORES"
 cmake -B build -DPNG_LIBRARY="/opt/homebrew/opt/libpng/lib"
 cmake --build ./build -- -j"$CORES"
 
-set -x
-
 install_name_tool -add_rpath @loader_path build/akhenaten.app/Contents/MacOS/akhenaten
 
-cp build/_deps/sdl2_mixer-build/libSDL2_mixer-2.0.801.0.0.dylib build/akhenaten.app/Contents/MacOS/
-
+find build/_deps/sdl2_mixer-build/ -type f ! -type l -name 'libSDL2*' -exec /bin/cp {} build/akhenaten.app/Contents/MacOS/ \;
