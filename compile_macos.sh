64467f15e8fc418476bda82de1562d94f78845ee
MacThings <info@sl-soft.de>
2025-11-27 18:19:57 +0100

    Fixed SDL2 Dylib linking
    and removed Brotli stuff from cmake .. was not necessary

--- compile_macos.sh
+++ compile_macos.sh
@@ -8,3 +8,10 @@ mkdir build
 cmake --build ./build --target clean -- -j"$CORES"
 cmake -B build -DPNG_LIBRARY="/opt/homebrew/opt/libpng/lib"
 cmake --build ./build -- -j"$CORES"
+
+set -x
+
+install_name_tool -add_rpath @loader_path build/akhenaten.app/Contents/MacOS/akhenaten
+
+cp build/_deps/sdl2_mixer-build/libSDL2_mixer-2.0.801.0.0.dylib build/akhenaten.app/Contents/MacOS/
+
