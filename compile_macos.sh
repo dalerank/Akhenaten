be49dd1a51f8f25f84b4c318a5e0a5b01baff43c
MacThings <info@sl-soft.de>
2025-11-27 19:13:59 +0100

    Rmoved SDL2 workaround

--- compile_macos.sh
+++ compile_macos.sh
@@ -8,7 +8,3 @@ mkdir build
 cmake --build ./build --target clean -- -j"$CORES"
 cmake -B build -DPNG_LIBRARY="/opt/homebrew/opt/libpng/lib"
 cmake --build ./build -- -j"$CORES"
-
-install_name_tool -add_rpath @loader_path build/akhenaten.app/Contents/MacOS/akhenaten
-
-find build/_deps/sdl2_mixer-build/ -type f ! -type l -name 'libSDL2*' -exec /bin/cp {} build/akhenaten.app/Contents/MacOS/ \;
