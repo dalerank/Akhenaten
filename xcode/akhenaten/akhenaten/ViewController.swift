253c5993fc0e148b264c2d5d13a3ab3ce34e7c95
MacThings <info@sl-soft.de>
2025-11-28 09:29:09 +0100

    Added innoextract

--- xcode/akhenaten/akhenaten/ViewController.swift
+++ xcode/akhenaten/akhenaten/ViewController.swift
@@ -6,8 +6,19 @@
 //
 
 import Cocoa
+import Foundation
+import AppKit
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var startButton: NSButton!
+    @IBOutlet weak var updatesButton: NSButton!
+    @IBOutlet weak var visitButton: NSButton!
+    @IBOutlet weak var installButton: NSButton!
+    @IBOutlet weak var installInfo: NSTextField!
+    
+    
+    let scriptPath = Bundle.main.path(forResource: "/innoextract/innoextract", ofType: "command")!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -76,5 +87,72 @@ class ViewController: NSViewController {
         NSWorkspace.shared.open(url)
     }
 
+    @IBAction func install_gog(_ sender: Any) {
+        requestGOGInstaller()
+        
+
+        
+    }
+    
+    /// MARK: EXE auswählen
+    private func requestGOGInstaller() {
+        let dialog = NSOpenPanel()
+        dialog.title = NSLocalizedString("SelectInstallerTitle", comment: "")
+        dialog.message = NSLocalizedString("SelectInstallerMessage", comment: "")
+        dialog.allowedFileTypes = ["exe"]
+        dialog.allowsMultipleSelection = false
+        dialog.canChooseDirectories = false
+        
+        dialog.begin { response in
+            guard response == .OK, let url = dialog.url else { return }
+            
+            // Validierung sicherstellen
+            guard FileManager.default.fileExists(atPath: url.path) else {
+                self.showError(NSLocalizedString("InstallerNotFound", comment: ""))
+                return
+            }
+            
+            // Speichern
+            UserDefaults.standard.set(url.path, forKey: "GOGInstaller")
+            
+            self.installInfo.isHidden = false
+            self.startButton.isEnabled = false
+            self.updatesButton.isEnabled = false
+            self.visitButton.isEnabled = false
+            self.installButton.isEnabled = false
+            
+            self.syncShellExec(path: self.scriptPath, args: ["gog_install"])
+            
+            self.installInfo.isHidden = true
+            self.startButton.isEnabled = true
+            self.updatesButton.isEnabled = true
+            self.visitButton.isEnabled = true
+            self.installButton.isEnabled = true
+        }
+        
+
+    }
+
+    /// MARK: Error Alert
+    private func showError(_ message: String) {
+        let alert = NSAlert()
+        alert.messageText = NSLocalizedString("This is no valid GOG Installer.", comment: "")
+        alert.informativeText = message
+        alert.alertStyle = .critical
+        alert.runModal()
+    }
+    
+    func syncShellExec(path: String, args: [String] = []) {
+        if let mainWindow = NSApplication.shared.windows.first {
+            mainWindow.makeFirstResponder(nil)
+        }
+        let process            = Process()
+        process.launchPath     = "/bin/bash"
+        process.arguments      = [path] + args
+        let outputPipe         = Pipe()
+        process.standardOutput = outputPipe
+        process.launch() // Start process
+        process.waitUntilExit() // Wait for process to terminate.
+    }
 }
 
