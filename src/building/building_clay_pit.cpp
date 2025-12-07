3abbf7b50fffad1578f4013e5e877afbb9a045fd
dalerank <dalerankn8@gmail.com>
2025-12-07 15:38:43 +0100

    improved clay pit spaw logic and resource storage

--- src/building/building_clay_pit.cpp
+++ src/building/building_clay_pit.cpp
@@ -3,9 +3,16 @@
 #include "building/building.h"
 #include "game/game_config.h"
 #include "city/city_finance.h"
+#include "city/city_resource.h"
 #include "widget/city/ornaments.h"
 #include "game/game_events.h"
 #include "js/js_game.h"
+#include "graphics/graphics.h"
+#include "graphics/image.h"
+#include "graphics/animation.h"
+#include "figure/figure.h"
+#include "game/resource.h"
+#include <cmath>
 
 REPLICATE_STATIC_PARAMS_FROM_CONFIG(building_clay_pit);
 
@@ -25,8 +32,53 @@ void building_clay_pit::on_before_flooded() {
     }
 }
 
+int building_clay_pit::stored_amount(e_resource r) const {
+    // For clay pit, resources are stored in stored_amount_first (like hunting_lodge and reed_gatherer)
+    if (r == RESOURCE_CLAY || r == base.output.resource) {
+        return base.stored_amount_first;
+    }
+    return building_industry::stored_amount(r);
+}
+
+void building_clay_pit::spawn_figure() {
+    check_labor_problem();
+    if (!has_road_access()) {
+        return;
+    }
+
+    common_spawn_labor_seeker(current_params().min_houses_coverage);
+            
+    figure* fcart = base.common_spawn_goods_output_cartpusher();
+    if (fcart) {
+        events::emit(event_produced_resources{ base.output.resource, fcart->get_carrying_amount() });
+    }
+}
+
+void building_clay_pit::production_finished() {
+    if (stored_amount(RESOURCE_CLAY) >= base.max_storage_amount(RESOURCE_CLAY)) {
+        return;
+    }
+
+    auto &d = runtime_data();
+    if (d.progress >= d.progress_max) {
+        base.stored_amount_first += ready_production();
+
+        d.progress = 0;
+        d.has_raw_materials = false;
+    }
+}
+
 bool building_clay_pit::draw_ornaments_and_animations_height(painter &ctx, vec2i point, tile2i tile, color color_mask) {
     draw_normal_anim(ctx, point, tile, color_mask);
+
+    int amount = ceil((float)stored_amount(RESOURCE_CLAY) / 100.0) - 1;
+    if (amount >= 0) {
+        const auto &ranim = anim(animkeys().clay);
+        auto& command = ImageDraw::create_subcommand(render_command_t::ert_generic);
+        command.image_id = ranim.first_img() + amount;
+        command.pixel = point + ranim.pos;
+        command.mask = color_mask;
+    }
 
     return true;
 }
