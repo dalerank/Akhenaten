9183fb01bc8fab05379525350365bfe5a851dec2
dalerank <dalerankn8@gmail.com>
2025-12-07 17:30:48 +0100

    buildings: mine copper add depletion logic

--- src/building/building_mine_copper.cpp
+++ src/building/building_mine_copper.cpp
@@ -6,6 +6,8 @@
 #include "city/city_finance.h"
 #include "game/game_events.h"
 #include "js/js_game.h"
+#include "grid/copper.h"
+#include "grid/grid.h"
 
 REPLICATE_STATIC_PARAMS_FROM_CONFIG(building_mine_copper);
 
@@ -17,6 +19,34 @@ bool building_mine_copper::preview::is_need_flag(build_planner &planer, e_buildi
     }
 
     return building_planer_renderer::is_need_flag(planer, flag);
+}
+
+void building_mine_copper::update_production() {
+    auto &d = runtime_data();
+    int current_progress = d.progress;
+
+    tile2i best_tile = tile2i::invalid;
+    int best_resource = 0;
+
+    grid_area search_area = map_grid_get_area(base.tile, base.size, 0);
+    map_grid_area_foreach(search_area.tmin, search_area.tmax, [&] (tile2i t) {
+        int resource = map_get_copper(t);
+        if (resource > 0 && resource > best_resource) {
+            best_tile = t;
+            best_resource = resource;
+        }
+    });
+
+    if (best_resource <= 0) {
+        return;
+    }
+    
+    building_industry::update_production();
+    int delta_progress = d.progress - current_progress;
+    
+    if (delta_progress > 0) {
+        map_copper_deplete(best_tile, delta_progress);
+    }
 }
 
 void building_mine_copper::on_before_collapse() {
