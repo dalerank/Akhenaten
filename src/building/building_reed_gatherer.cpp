cd850c608add343450dbddb68e146c845578afb1
dalerank <dalerankn8@gmail.com>
2025-12-07 13:30:41 +0100

    building: reed gatherer show reeds in store amount

--- src/building/building_reed_gatherer.cpp
+++ src/building/building_reed_gatherer.cpp
@@ -7,6 +7,9 @@
 #include "core/random.h"
 #include "game/game_events.h"
 #include "city/city_resource.h"
+#include "widget/city/ornaments.h"
+#include "graphics/graphics.h"
+#include "graphics/image.h"
 #include "js/js_game.h"
 
 REPLICATE_STATIC_PARAMS_FROM_CONFIG(building_reed_gatherer);
@@ -36,8 +39,22 @@ bool building_reed_gatherer::can_spawn_gatherer(int max_gatherers_per_building, 
     return false;
 }
 
+bool building_reed_gatherer::can_play_animation() const {
+    return building_industry::can_play_animation();
+}
+
 bool building_reed_gatherer::draw_ornaments_and_animations_height(painter &ctx, vec2i point, tile2i tile, color color_mask) {
     draw_normal_anim(ctx, point, tile, color_mask);
+
+    int amount = std::min<int>(1, ceil((float)base.stored_amount() / 100.0) - 1);
+    if (amount >= 0) {
+        const auto &ranim = anim(animkeys().reeds);
+
+        auto& command = ImageDraw::create_subcommand(render_command_t::ert_generic);
+        command.image_id = ranim.first_img() + amount;
+        command.pixel = point + ranim.pos;
+        command.mask = color_mask;
+    }
 
     return true;
 }
