3d55c3d8704b357d34e498c7bde9342200c65716
MacThings <info@sl-soft.de>
2025-12-07 15:23:36 +0100

    Fixed SDL2 compiler warnings

--- compile_macos.sh
+++ compile_macos.sh
@@ -2,8 +2,8 @@
 
 CORES=$(sysctl -n hw.ncpu)
 
-rm -rf build 2>/dev/null
-mkdir build
+#rm -rf build 2>/dev/null
+#mkdir build
 
 cmake --build ./build --target clean -- -j"$CORES"
 cmake -B build -DPNG_LIBRARY="/opt/homebrew/opt/libpng/lib"
