b2bfe7c88921934f39223e83b0767e137dd185b5
MacThings <info@sl-soft.de>
2025-12-07 14:05:29 +0100

    Fixed some compiler warnings with help of AI

--- src/lame/libmp3lame/VbrTag.c
+++ src/lame/libmp3lame/VbrTag.c
@@ -251,7 +251,7 @@ IsVbrTag(const unsigned char *buf)
     return (isTag0 || isTag1);
 }
 
-#define SHIFT_IN_BITS_VALUE(x,n,v) ( x = (x << (n)) | ( (v) & ~(-1 << (n)) ) )
+#define SHIFT_IN_BITS_VALUE(x,n,v) ( x = ((x) << (n)) | ((uint32_t)(v) & ((1u << (n)) - 1u)) )
 
 static void
 setLameTagFrameHeader(lame_internal_flags const *gfc, unsigned char *buffer)
@@ -427,12 +427,11 @@ GetVbrTag(VBRTAGDATA * pTagData, const unsigned char *buf)
     }
 
     if (head_flags & TOC_FLAG) {
-        if (pTagData->toc != NULL) {
-            for (i = 0; i < NUMTOCENTRIES; i++)
-                pTagData->toc[i] = buf[i];
-        }
-        buf += NUMTOCENTRIES;
+    for (i = 0; i < NUMTOCENTRIES; i++) {
+        pTagData->toc[i] = buf[i];
     }
+    		buf += NUMTOCENTRIES;
+		}	
 
     pTagData->vbr_scale = -1;
 
