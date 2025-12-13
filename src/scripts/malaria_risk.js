26c40f47b5bc6a66a3ff638de13b76ab91422c33
dalerank <dalerankn8@gmail.com>
2025-12-13 17:39:07 +0100

    fix: add missing config for malaria risk grid

---
+++ src/scripts/malaria_risk.js
@@ -0,0 +1,24 @@
+log_info("akhenaten: malaria_risk config started")
+
+malaria_risk {
+    max_risk : 100
+    max_range : 6
+
+    water_risk : 0
+    water_step_delta : 0
+    water_risk_range : 0
+
+    floodplain_risk : 5
+    floodplain_step_delta : -1
+    floodplain_risk_range : 4
+
+    marshland_risk : 6
+    marshland_step_delta : -1
+    marshland_risk_range : 6
+
+    tree_risk : -2
+    tree_step_delta : 0
+    tree_risk_range : 3
+
+    outside_map_risk : 0
+}
