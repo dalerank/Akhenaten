64f66aa1c1dc508863bba8f848752e68b93905cb
dalerank <dalerankn8@gmail.com>
2025-12-30 01:32:19 +0100

    ui: military advisor draw battalions logic moved to js from hardcode

--- src/window/advisor/advisor_military.cpp
+++ src/window/advisor/advisor_military.cpp
@@ -17,42 +17,19 @@
 #include "window/window_city.h"
 #include "game/game.h"
 
+#include "js/js_game.h"
+#include "graphics/elements/ui_js.h"
+
 ui::advisor_military_window g_advisor_military_window;
 
 static void button_go_to_legion(int legion_id, int param2);
 static void button_return_to_fort(int legion_id, int param2);
 static void button_empire_service(int legion_id, int param2);
-static void on_scroll();
 
-static scrollbar_t g_advisor_mil_scrollbar = {{592, 70}, 272, on_scroll};
-
-static generic_button fort_buttons[] = {
-  {384, 83, 30, 30, button_go_to_legion, button_none, 1, 0},
-  {464, 83, 30, 30, button_return_to_fort, button_none, 1, 0},
-  {544, 83, 30, 30, button_empire_service, button_none, 1, 0},
-  {384, 127, 30, 30, button_go_to_legion, button_none, 2, 0},
-  {464, 127, 30, 30, button_return_to_fort, button_none, 2, 0},
-  {544, 127, 30, 30, button_empire_service, button_none, 2, 0},
-  {384, 171, 30, 30, button_go_to_legion, button_none, 3, 0},
-  {464, 171, 30, 30, button_return_to_fort, button_none, 3, 0},
-  {544, 171, 30, 30, button_empire_service, button_none, 3, 0},
-  {384, 215, 30, 30, button_go_to_legion, button_none, 4, 0},
-  {464, 215, 30, 30, button_return_to_fort, button_none, 4, 0},
-  {544, 215, 30, 30, button_empire_service, button_none, 4, 0},
-  {384, 259, 30, 30, button_go_to_legion, button_none, 5, 0},
-  {464, 259, 30, 30, button_return_to_fort, button_none, 5, 0},
-  {544, 259, 30, 30, button_empire_service, button_none, 5, 0},
-  {384, 303, 30, 30, button_go_to_legion, button_none, 6, 0},
-  {464, 303, 30, 30, button_return_to_fort, button_none, 6, 0},
-  {544, 303, 30, 30, button_empire_service, button_none, 6, 0},
-};
-
-static int focus_button_id;
-static int num_legions;
+struct advisor_military_window_draw { vec2i pos; };
+ANK_REGISTER_STRUCT_WRITER(advisor_military_window_draw, pos);
 
 void ui::advisor_military_window::init() {
-    num_legions = formation_get_num_forts();
-    g_advisor_mil_scrollbar.init(0, num_legions - 6);
 }
 
 int ui::advisor_military_window::draw_background(UiFlags flags) {
@@ -73,6 +50,7 @@ int ui::advisor_military_window::draw_background(UiFlags flags) {
     image_desc group_bullet{PACK_GENERAL, 158};
 
     vec2i pos = ui["forts_area"].pos;
+    int num_legions = formation_get_num_forts();
     if (num_legions <= 0) {
         ui["enemy_text"].pos.y = pos.y + 10;
         ui["distant_text"].pos.y = pos.y + 30;
@@ -96,74 +74,31 @@ int ui::advisor_military_window::draw_background(UiFlags flags) {
 void ui::advisor_military_window::ui_draw_foreground(UiFlags flags) {
     ui.begin_widget(pos);
     ui.draw();
-
-    if (num_legions > 0) {
-        for (int i = 0; i < 6 && i < num_legions; i++) {
-            const formation *form = formation_get(formation_for_legion(i + 1 + g_advisor_mil_scrollbar.scroll_position));
-            ui.button("", vec2i{ 22, 77 + 44 * i }, vec2i{ 560, 40 }, fonts_vec{ FONT_NORMAL_BLACK_ON_DARK });
-            ui.image({ PACK_GENERAL, 127, form->batalion_id }, vec2i{ 32, 82 + 44 * i });
-            ui.label(ui::str(138, form->batalion_id), vec2i{ 84, 83 + 44 * i }, FONT_NORMAL_WHITE_ON_DARK);
-
-            pcstr type_str = "";
-            switch (form->figure_type) {
-            case FIGURE_INFANTRY: type_str = ui::str(138, 33); break;
-            case FIGURE_FCHARIOTEER: type_str = ui::str(138, 34); break;
-            case FIGURE_ARCHER: type_str = ui::str(138, 35); break;
-            }
-            ui.label(bstring64().printf("%u %s", form->num_figures, type_str), vec2i{ 84, 100 + 44 * i }, FONT_NORMAL_BLACK_ON_DARK);
-            ui.label(ui::str(138, 37 + form->morale / 5), vec2i{224, 91 + 44 * i}, FONT_NORMAL_BLACK_ON_DARK, UiFlags_AlignCentered, 150);
-
-            image_desc image{ PACK_GENERAL, 222 };
-            ui.button("", vec2i{ 384, 83 + 44 * i }, vec2i{ 30, 30 }, fonts_vec{ FONT_NORMAL_BLACK_ON_DARK });
-            ui.image(image + 0, vec2i{ 387, 86 + 44 * i });
-
-            ui.button("", vec2i{ 464, 83 + 44 * i }, vec2i{ 30, 30 });
-            ui.image(image + (form->is_at_fort ? 2 : 1), vec2i{ 467, 86 + 44 * i });
-
-            ui.button("", vec2i{ 544, 83 + 44 * i }, vec2i{ 30, 30 });
-            ui.image(image + (form->empire_service ? 3 : 4), vec2i{ 547, 86 + 44 * i });
-        }
-    }
+    ui.draw(advisor_military_window_draw{ pos });
     ui.end_widget();
-
-    scrollbar_draw(vec2i{0, 0}, &g_advisor_mil_scrollbar);
-    //num_legions = formation_get_num_forts();
-    //for (int i = 0; i < 6 && i < num_legions; i++) {
-    //    button_border_draw(384, 83 + 44 * i, 30, 30, focus_button_id == 3 * i + 1);
-    //    button_border_draw(464, 83 + 44 * i, 30, 30, focus_button_id == 3 * i + 2);
-    //    button_border_draw(544, 83 + 44 * i, 30, 30, focus_button_id == 3 * i + 3);
-    //}
 }
 
 int ui::advisor_military_window::handle_mouse(const mouse* m) {
-    if (scrollbar_handle_mouse(vec2i{0, 0}, &g_advisor_mil_scrollbar, m)) {
-        return 1;
-    }
-
     return 0;
 }
 
 static void button_go_to_legion(int legion_id, int param2) {
-    const formation* m = formation_get(formation_for_legion(legion_id + g_advisor_mil_scrollbar.scroll_position));
+    const formation* m = formation_get(g_formations.get_battalion_id_from_index(legion_id));
     camera_go_to_mappoint(m->home);
     window_city_show();
 }
 
 static void button_return_to_fort(int legion_id, int param2) {
-    formation* m = formation_get(formation_for_legion(legion_id + g_advisor_mil_scrollbar.scroll_position));
+    formation* m = formation_get(g_formations.get_battalion_id_from_index(legion_id));
     if (!m->in_distant_battle) {
         formation_batalion_return_home(m);
     }
 }
 
 static void button_empire_service(int legion_id, int param2) {
-    int formation_id = formation_for_legion(legion_id + g_advisor_mil_scrollbar.scroll_position);
+    int formation_id = g_formations.get_battalion_id_from_index(legion_id);
     formation_toggle_empire_service(formation_id);
     formation_calculate_figures();
-}
-
-static void on_scroll(void) {
-
 }
 
 advisor_window* ui::advisor_military_window::instance() {
