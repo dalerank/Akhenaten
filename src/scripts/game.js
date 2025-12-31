020f858b10290e67adb9a3e6f6034173e128ad27
dalerank <dalerankn8@gmail.com>
2025-12-31 20:40:34 +0100

    js: simplified properties in game class

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,20 +1,13 @@
 log_info("akhenaten: game.js started")
 
 game {
-    [property]
-    absolute_day { get: __game_absolute_day }
-
-    [property]
-    version { get: __game_version }
+    @absolute_day { get: __game_absolute_day }
+    @version { get: __game_version }
 
     screen {
-        [property]
-        w { get: __game_screen_width }
-
-        [property]
-        h { get: __game_screen_height }
+        @w { get: __game_screen_width }
+        @h { get: __game_screen_height }
     }
 
-    [property]
-    difficulty { get: __game_difficulty }
+    @difficulty { get: __game_difficulty }
 }
