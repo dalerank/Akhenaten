d843870ea0aac4fcd89810acdf6eb19fab6eb5b5
dalerank <dalerankn8@gmail.com>
2026-01-01 22:11:18 +0100

    mods: support russian voices as mod

--- src/scripts/mods.js
+++ src/scripts/mods.js
@@ -50,5 +50,14 @@ mods {
             url : "https://f003.backblazeb2.com/file/akhenaten-mods/pharaoh_italian_voices.sgx"
             email : "dalerankn8@gmail.com"
         }
+
+        {
+            name : "pharaoh_russian_voices"
+            desc : "Russian voices for the game"
+            version : "1.0.0"
+            author : "dalerank"
+            url : "https://f003.backblazeb2.com/file/akhenaten-mods/pharaoh_russian_voices.sgx"
+            email : "dalerankn8@gmail.com"
+        }
     ]
 }
