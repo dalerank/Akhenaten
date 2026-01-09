4c22e91ca9f0bd9c4dd3abfaf2ee7746cb8fd7c0
dalerank <dalerankn8@gmail.com>
2026-01-09 16:19:49 +0100

    js: add converter for e_advisor type

--- src/window/window_advisors_js.cpp
+++ src/window/window_advisors_js.cpp
@@ -3,3 +3,11 @@
 #include "js/js_game.h"
 
 ANK_FUNCTION(window_advisors_show_checked)
+
+namespace js_helpers {
+    template<>
+    inline e_advisor js_to_value<e_advisor>(js_State *J, int idx) {
+        return (e_advisor)js_tointeger(J, idx);
+    }
+}
+ANK_FUNCTION_1(window_advisors_show_advisor)
