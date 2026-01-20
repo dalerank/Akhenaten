8caec93c5786aa957e1c4d068ee3bda92b2bb093
dalerank <dalerankn8@gmail.com>
2026-01-20 23:26:56 +0100

    buildings: add option is_beautification to config

--- src/building/building_fwd.h
+++ src/building/building_fwd.h
@@ -112,10 +112,12 @@ struct building_flags_rule {
     bool is_administration;
     bool is_water_crossing;
     bool is_infrastructure;
+    bool is_beautification;
 };
 ANK_CONFIG_STRUCT(building_flags_rule,
     is_monument, is_extractor, is_harvester, is_farm, is_fort, is_education, is_palace, is_temple,
-    is_shrine, is_tax_collector, is_statue, is_administration, is_water_crossing, is_infrastructure)
+    is_shrine, is_tax_collector, is_statue, is_administration, is_water_crossing, is_infrastructure,
+    is_beautification)
 
 struct building_crime_t {
     svector<int8_t, 6> value;
