e95ea9cbdc89b149597ba1d8cb48da162160a285
dalerank <dalerankn8@gmail.com>
2026-01-24 11:55:35 +0100

    console: moved events history handler from header

--- src/dev/qconsole.h
+++ src/dev/qconsole.h
@@ -8,8 +8,7 @@
 #include <sstream>
 #include <memory>
 #include <string>
-
-#include "game/game_events_history.h"
+#include <algorithm>
 
 namespace dev
 {
@@ -591,19 +590,6 @@ inline void dev::qconsole::listCVars(std::ostream &os) const {
 
     for (CVarReadTable::const_iterator it = cvarReadFTable.begin(); it != cvarReadFTable.end(); it++) {
         os << "\n" << it->first;
-    }
-
-    os << std::endl;
-}
-
-inline void dev::qconsole::eventsHistory(std::ostream &os) const
-{
-    os << "\nEvents history:";
-
-    const auto &lines = events_history::event_history;
-    for (int i = 0; i < lines.events.size(); ++i)
-    {
-        os << events_history::_event_to_string(lines.events[i]).c_str() << std::endl;
     }
 
     os << std::endl;
--- src/game/game_events_history.h
+++ src/game/game_events_history.h
@@ -11,40 +11,28 @@ namespace events_history {
     {
         time_t timestamp;
         xstring name;
-        xstring source;
         xstring description;
+        pcstr source;
     };
 
     struct event_history_s
     {
         circular_buffer<event_history_record_s, 8> events;
     };
-    extern event_history_s event_history;
+        
+    const event_history_s& get_event_history();
+    event_history_s& ref_event_history();
 
-    inline void _append_record(const event_history_record_s &record)
-    {
-        event_history.events.write_tail(record);
-    }
+    void _append_record(const event_history_record_s &record);
 
-    inline const bstring1024& _event_to_string(const event_history_record_s &record)
-    {
-        const tm *loctime = localtime(&record.timestamp);
-        bstring32 timestamp;
-        strftime(timestamp, bstring32::capacity, "%FT%TZ", loctime);
-        static bstring1024 event_str;
-        event_str.append(timestamp);
-        event_str.append_fmt(" %s", record.name.c_str());
-        event_str.append_fmt(" %s", record.source.c_str());
-        event_str.append_fmt(" %s", record.description.c_str());
-        return event_str;
-    }
+    const bstring1024& _event_to_string(const event_history_record_s &record);
 
     template<typename EventType>
     void log_event (EventType const& ev)
     {
         const char* event_name = typeid(EventType).name();
         // TODO: Replace placeholders with actual source and misc info
-        const event_history_record_s record = {time(nullptr), event_name, xstring(), xstring()};
-        event_history.events.write_tail(record);
+        const event_history_record_s record = {time(nullptr), event_name, xstring(), ""};
+        ref_event_history().events.write_tail(record);
     }
 }
