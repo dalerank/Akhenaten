b37fd81183c30b1abed6cafb4e3871b5438f8801
dalerank <dalerankn8@gmail.com>
2026-01-25 21:30:34 +0100

    city: add labor category for houses

--- src/city/labor_category.h
+++ src/city/labor_category.h
@@ -3,6 +3,7 @@
 #include "core/tokenum.h"
 
 enum e_labor_category : int8_t {
+    LABOR_CATEGORY_INVALID = -1,
     LABOR_CATEGORY_NONE = -1,
     LABOR_CATEGORY_FOOD_PRODUCTION = 0, // todo: wrong index...
     LABOR_CATEGORY_INDUSTRY_COMMERCE = 1,
@@ -14,9 +15,10 @@ enum e_labor_category : int8_t {
     LABOR_CATEGORY_GOVERNMENT = 7,
     LABOR_CATEGORY_MILITARY = 8,
     LABOR_CATEGORY_CULTURE = 9,
-    LABOR_CATEGORY_SIZE = 10,
+    LABOR_CATEGORY_HOUSE = 10,
 
-    LABOR_CATEGORY_INVALID = -1
+    LABOR_CATEGORY_SIZE,
 };
 
-extern const token_holder<e_labor_category, LABOR_CATEGORY_NONE, LABOR_CATEGORY_SIZE> e_labor_category_tokens;
+using e_labor_category_tokens_t = token_holder<e_labor_category, LABOR_CATEGORY_NONE, LABOR_CATEGORY_SIZE>;
+extern const e_labor_category_tokens_t e_labor_category_tokens;
