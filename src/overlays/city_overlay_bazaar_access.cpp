3306057222f4bdfc02171008c339f220e2e2bbc9
m-anvl <mamaev-av18@ya.ru>
2026-01-30 23:10:30 +0400

    fix: overlay bazaar access, get correct tooltip

--- src/overlays/city_overlay_bazaar_access.cpp
+++ src/overlays/city_overlay_bazaar_access.cpp
@@ -1,5 +1,6 @@
 #include "city_overlay_bazaar_access.h"
 
+#include "core/calc.h"
 #include "grid/floodplain.h"
 #include "building/building_house.h"
 #include "figure/figure.h"
@@ -30,5 +31,34 @@ int city_overlay_bazaar_access::get_column_height(const building *b) const {
 }
 
 xstring city_overlay_bazaar_access::get_tooltip_for_building(tooltip_context *c, const building *b) {
-    return ui::str(66, 58);
+    auto house = ((building *)b)->dcast_house();
+
+    if (!house) {
+        return ui::str(66, 7);
+    }
+
+    auto &housed = house->runtime_data();
+    if (housed.population <= 0) {
+        return {};
+    }
+
+    if (!house->model().food_types) {
+        return ui::str(66, 104);
+    } else {
+        int stocks_present = 0;
+        for (int i = INVENTORY_MIN_FOOD; i < INVENTORY_MAX_FOOD; i++) {
+            stocks_present += housed.foods[i];
+        }
+
+        int stocks_per_pop = calc_percentage<int>(stocks_present, housed.population);
+        if (stocks_per_pop <= 0) {
+            return ui::str(66, 4);
+        } else if (stocks_per_pop < 100) {
+            return ui::str(66, 5);
+        } else if (stocks_per_pop <= 200) {
+            return ui::str(66, 6);
+        } else {
+            return ui::str(66, 7);
+        }
+    }
 }
