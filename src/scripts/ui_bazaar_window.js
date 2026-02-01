2cba2eeea9a10d0392775eb6e24ebb37a16caa65
dalerank <dalerankn8@gmail.com>
2026-02-01 01:36:28 +0100

    ui: moved bazaar config before functions

--- src/scripts/ui_bazaar_window.js
+++ src/scripts/ui_bazaar_window.js
@@ -36,6 +36,25 @@ bazaar_info_window {
     }
 }
 
+bazaar_orders_window {
+ui {
+        background   : outer_panel({size[29, 17]}),
+        title        : text({pos[0, 12], size[px(28), 0], text:{group:98, id:5}, font : FONT_LARGE_BLACK_ON_LIGHT, align:"center"})
+        orders_panel : inner_panel({pos[16, 42], size[27, 11] })
+        accept_none  : button({pos[80, -1], size[300, 24], text:{group:99, id:7}, margin{bottom:-38} })
+
+        item_orders_column : dummy({margin{centerx:0}})
+        item_icon_column : dummy({pos[25, 0]})
+        item_name_column : dummy({pos[55, 0], font:FONT_NORMAL_WHITE_ON_DARK })
+        item_row     : dummy({size[px(11), 20]})
+        items_area   : dummy({pos[0, 50]})
+        item_icon_column_2 : dummy({pos[px(29) - 40, 0]})
+
+        button_help   : help_button({})
+        button_close  : close_button({})
+    }
+}
+
 [es=bazaar_info_window_init]
 function bazaar_info_window_init(window) {
     var bazaar = city.get_bazaar(window.bid)
@@ -113,23 +132,4 @@ function bazaar_info_window_draw_goods(window) {
     draw_good(bazaar, RESOURCE_LUXURY_GOODS, 5, window.good1_icon, window.good1_text)
     draw_good(bazaar, RESOURCE_LINEN, 6, window.good2_icon, window.good2_text)
     draw_good(bazaar, RESOURCE_BEER, 7, window.good3_icon, window.good3_text)
-}
-
-bazaar_orders_window {
-  ui {
-        background   : outer_panel({size[29, 17]}),
-        title        : text({pos[0, 12], size[px(28), 0], text:{group:98, id:5}, font : FONT_LARGE_BLACK_ON_LIGHT, align:"center"})
-        orders_panel : inner_panel({pos[16, 42], size[27, 11] })
-        accept_none  : button({pos[80, -1], size[300, 24], text:{group:99, id:7}, margin{bottom:-38} })
-
-        item_orders_column : dummy({margin{centerx:0}})
-        item_icon_column : dummy({pos[25, 0]})
-        item_name_column : dummy({pos[55, 0], font:FONT_NORMAL_WHITE_ON_DARK })
-        item_row     : dummy({size[px(11), 20]})
-        items_area   : dummy({pos[0, 50]})
-        item_icon_column_2 : dummy({pos[px(29) - 40, 0]})
-
-        button_help   : help_button({})
-        button_close  : close_button({})
-    }
 }
