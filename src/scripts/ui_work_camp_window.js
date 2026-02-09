3caaeac337e8e9cc033606fc72449a211041bde8
dalerank <dalerankn8@gmail.com>
2026-02-09 17:04:06 +0100

    ui: work camp info window moved to scripts

--- src/scripts/ui_work_camp_window.js
+++ src/scripts/ui_work_camp_window.js
@@ -1,8 +1,19 @@
 log_info("akhenaten: work camp window started")
 
-function work_camp_warning_text() {
-    var b = city.get_building(city.object_info.building_id)
-    var reason = { group:city.object_info.group, id:0 }
+
+[es=building_info_window]
+info_window_work_camp {
+    related_buildings [BUILDING_WORK_CAMP]
+    ui : baseui(building_info_window, {
+        workers_desc  : text({ pos[70, 124 + 20], font: FONT_NORMAL_BLACK_ON_DARK, wrap:px(24), multiline:true })
+        warning_text  : text({ pos[32, 46], wrap:px(26), font : FONT_NORMAL_BLACK_ON_LIGHT, multiline:true })
+    })
+}
+
+[es=info_window_work_camp_init]
+function info_window_work_camp_warning_text(window) {
+    var b = city.get_building(window.bid)
+    var reason = { group:b.meta_text_id, id:0 }
     if (b.has_road_access == false) {
         reason = { group:69, id:25 }
     } else if (b.num_workers == 0) {
@@ -22,20 +33,8 @@ function work_camp_warning_text() {
     if (reason.id) {
         warning_text += __loc(reason)
     }
-    return warning_text
-}
+    window.warning_text.text = warning_text
 
-function work_camp_workers_description() {
-    var b = city.get_building(city.object_info.building_id)
     var workers_desc = approximate_value(b.worker_percentage / 100.0, [8, 7, 6, 5, 4])
-    return __loc(179, workers_desc)
-}
-
-info_window_work_camp {
-    ui : baseui(building_info_window, {
-        workers_desc  : text({ pos[70, 124 + 20], font: FONT_NORMAL_BLACK_ON_DARK, wrap:px(24), multiline:true
-                               textfn:work_camp_workers_description })
-        warning_text  : text({ pos[32, 46], wrap:px(26), font : FONT_NORMAL_BLACK_ON_LIGHT, multiline:true
-                               textfn:work_camp_warning_text })
-    })
+    window.workers_desc.text = __loc(179, workers_desc)
 }
