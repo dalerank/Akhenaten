1864167ea30e949923b2f93ccfcb1ceff4d45240
dalerank <dalerankn8@gmail.com>
2026-02-21 00:11:04 +0100

    refactor: removed hardcoded logic from window terrain info

--- src/scripts/ui_terrain_info.js
+++ src/scripts/ui_terrain_info.js
@@ -11,54 +11,98 @@ terrain_info_window = {
     }
 }
 
-terrain_road_info_window = {
-    help_id           : 57 
+terrain_info_road {
+    help_id           : 57
 	ui : baseui(terrain_info_window, {
         title         : text({pos: [0, 16], size: [px(29), 13], text:[28, 5], font:FONT_LARGE_BLACK_ON_LIGHT, align:"center"})
         describe      : text({pos: [30, 78], text:[70, 42], font: FONT_NORMAL_BLACK_ON_DARK, multiline:true, wrap:px(26) })
     })
 }
 
-terrain_wall_info_window = {
-    help_id           : 85 
-    open_sounds       : [ "Wavs/wall.wav" ] 
+[es=terrain_info_road_init]
+function terrain_info_road_on_init(window) {
+
+}
+
+terrain_info_wall = {
+    help_id           : 85
+    open_sounds       : [ "Wavs/wall.wav" ]
     ui : baseui(terrain_info_window, {
         title         : text({pos: [0, 16], size: [px(29), 13], text:[139, 0], font:FONT_LARGE_BLACK_ON_LIGHT, align:"center"})
         describe      : text({pos: [30, 78], text:[139, 1], font: FONT_NORMAL_BLACK_ON_DARK, multiline:true, wrap:px(26) })
     })
 }
 
-terrain_plaza_info_window = {
-    help_id           : 80 
-    open_sounds       : [ ] 
+[es=terrain_info_wall_init]
+function terrain_info_wall_on_init(window) {
+
+}
+
+terrain_info_plaza = {
+    help_id           : 80
+    open_sounds       : [ ]
     ui : baseui(terrain_info_window, {
         title         : text({pos: [0, 16], size: [px(29), 13], text:[137, 0], font:FONT_LARGE_BLACK_ON_LIGHT, align:"center"})
         describe      : text({pos: [30, 78], text:[137, 1], font: FONT_NORMAL_BLACK_ON_DARK, multiline:true, wrap:px(26) })
     })
 }
 
-terrain_orerock_info_window = {
-    help_id           : 191 
-    open_sounds       : [  "wavs/rock1.wav", "wavs/rock2.wav", "wavs/rock3.wav", "wavs/rock4.wav", "wavs/rock5.wav" ] 
+[es=terrain_info_plaza_init]
+function terrain_info_plaza_on_init(window) {
+
+}
+
+terrain_info_ore_rock = {
+    help_id           : 191
+    open_sounds       : [  "wavs/rock1.wav", "wavs/rock2.wav", "wavs/rock3.wav", "wavs/rock4.wav", "wavs/rock5.wav" ]
     ui : baseui(terrain_info_window, {
         title         : text({pos: [0, 16], size: [px(29), 13], text:[70, 26], font:FONT_LARGE_BLACK_ON_LIGHT, align:"center"})
         describe      : text({pos: [30, 78], text:[70, 38], font: FONT_NORMAL_BLACK_ON_DARK, multiline:true, wrap:px(26) })
     })
 }
 
-terrain_rock_info_window = {
-    help_id           : 191 
-    open_sounds       : [ "wavs/rock1.wav", "wavs/rock2.wav", "wavs/rock3.wav", "wavs/rock4.wav", "wavs/rock5.wav" ] 
+[es=terrain_info_ore_rock_init]
+function terrain_info_ore_rock_on_init(window) {
+
+}
+
+terrain_info_rock = {
+    help_id           : 191
+    open_sounds       : [ "wavs/rock1.wav", "wavs/rock2.wav", "wavs/rock3.wav", "wavs/rock4.wav", "wavs/rock5.wav" ]
     ui : baseui(terrain_info_window, {
         title         : text({pos: [0, 16], size: [px(29), 13], text:[70, 12], font:FONT_LARGE_BLACK_ON_LIGHT, align:"center"})
         describe      : text({pos: [30, 78], text:[70, 38], font: FONT_NORMAL_BLACK_ON_DARK, multiline:true, wrap:px(26) })
     })
 }
 
-terrain_floodplain_info_window = {
-    help_id           : 45 
+[es=terrain_info_rock_init]
+function terrain_info_rock_on_init(window) {
+
+}
+
+terrain_info_floodplain {
+    help_id           : 45
     ui : baseui(terrain_info_window, {
         title         : text({pos: [0, 16], size: [px(29), 13], text:[70, 29], font:FONT_LARGE_BLACK_ON_LIGHT, align:"center"})
         describe      : text({pos: [30, 78], text:[70, 55], font: FONT_NORMAL_BLACK_ON_DARK, multiline:true, wrap:px(26) })
     })
+}
+
+[es=terrain_info_floodplain_init]
+function terrain_info_floodplain_on_init(window) {
+
+}
+
+terrain_info_tree = {
+    help_id           : 44
+    open_sounds       : [ ]
+    ui : baseui(terrain_info_window, {
+        title         : text({pos: [0, 16], size: [px(29), 13], text:[70, 11], font:FONT_LARGE_BLACK_ON_LIGHT, align:"center"})
+        describe      : text({pos: [30, 78], text:[70, 43], font: FONT_NORMAL_BLACK_ON_DARK, multiline:true, wrap:px(26) })
+    })
+}
+
+[es=terrain_info_tree_init]
+function terrain_info_tree_on_init(window) {
+
 }
