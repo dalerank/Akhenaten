11bd885b064059b2616be96ca1dd4b8973062fbb
dalerank <dalerankn8@gmail.com>
2026-03-13 00:25:57 +0100

    js: redesigned jsV_getownproperty logic

--- src/mujs/jsvalue.h
+++ src/mujs/jsvalue.h
@@ -7,139 +7,135 @@ typedef struct js_FunctionModifier js_FunctionModifier;
 
 /* Hint to ToPrimitive() */
 enum {
-	JS_HNONE,
-	JS_HNUMBER,
-	JS_HSTRING
+    JS_HNONE,
+    JS_HNUMBER,
+    JS_HSTRING
 };
 
 enum js_Type {
-	JS_TSHRSTR, /* type tag doubles as string zero-terminator */
-	JS_TUNDEFINED,
-	JS_TNULL,
-	JS_TBOOLEAN,
-	JS_TNUMBER,
-	JS_TLITSTR,
-	JS_TMEMSTR,
-	JS_TOBJECT,
+    JS_TSHRSTR, /* type tag doubles as string zero-terminator */
+    JS_TUNDEFINED,
+    JS_TNULL,
+    JS_TBOOLEAN,
+    JS_TNUMBER,
+    JS_TLITSTR,
+    JS_TMEMSTR,
+    JS_TOBJECT,
 };
 
 enum js_Class {
-	JS_COBJECT,
-	JS_CARRAY,
-	JS_CFUNCTION,
-	JS_CSCRIPT, /* function created from global/eval code */
-	JS_CCFUNCTION, /* built-in function */
-	JS_CERROR,
-	JS_CBOOLEAN,
-	JS_CNUMBER,
-	JS_CSTRING,
-	JS_CREGEXP,
-	JS_CDATE,
-	JS_CMATH,
-	JS_CJSON,
-	JS_CITERATOR,
-	JS_CUSERDATA,
+    JS_COBJECT,
+    JS_CARRAY,
+    JS_CFUNCTION,
+    JS_CSCRIPT, /* function created from global/eval code */
+    JS_CCFUNCTION, /* built-in function */
+    JS_CERROR,
+    JS_CBOOLEAN,
+    JS_CNUMBER,
+    JS_CSTRING,
+    JS_CREGEXP,
+    JS_CDATE,
+    JS_CMATH,
+    JS_CJSON,
+    JS_CITERATOR,
+    JS_CUSERDATA,
 };
 
 /*
-	Short strings abuse the js_Value struct. By putting the type tag in the
-	last byte, and using 0 as the tag for short strings, we can use the
-	entire js_Value as string storage by letting the type tag serve double
-	purpose as the string zero terminator.
+    Short strings abuse the js_Value struct. By putting the type tag in the
+    last byte, and using 0 as the tag for short strings, we can use the
+    entire js_Value as string storage by letting the type tag serve double
+    purpose as the string zero terminator.
 */
 
-struct js_Value
-{
-	union {
-		int boolean;
-		double number;
-		char shrstr[8];
-		const char *litstr;
-		js_String *memstr;
-		js_Object *object;
-	} u;
-	char pad[7]; /* extra storage for shrstr */
-	char type; /* type tag and zero terminator for shrstr */
+struct js_Value {
+    union {
+        int boolean;
+        double number;
+        char shrstr[8];
+        const char *litstr;
+        js_String *memstr;
+        js_Object *object;
+    } u;
+    char pad[7]; /* extra storage for shrstr */
+    char type; /* type tag and zero terminator for shrstr */
 };
 
-struct js_String
-{
-	js_String *gcnext;
-	char gcmark;
-	char p[1];
+struct js_String {
+    js_String *gcnext;
+    char gcmark;
+    char p[1];
 };
 
-struct js_Regexp
-{
-	void *prog;
-	const char *source;
-	unsigned short flags;
-	unsigned short last;
+struct js_Regexp {
+    void *prog;
+    const char *source;
+    unsigned short flags;
+    unsigned short last;
 };
 
-struct js_Object
-{
-	enum js_Class type;
-	int extensible;
-	js_Property *properties;
-	js_Property *head, **tailp; /* for enumeration */
-	int count; /* number of properties, for array sparseness check */
-	js_Object *prototype;
-	union {
-		int boolean;
-		double number;
-		struct {
-			const char *string;
-			int length;
-		} s;
-		struct {
-			int length;
-		} a;
-		struct {
-			js_Function *function;
-			js_Environment *scope;
-		} f;
-		struct {
-			const char *name;
-			js_CFunction function;
-			js_CFunction constructor;
-			int length;
-		} c;
-		js_Regexp r;
-		struct {
-			js_Object *target;
-			js_Iterator *head;
-		} iter;
-		struct {
-			const char *tag;
-			void *data;
-			js_HasProperty has;
-			js_Put put;
-			js_Delete rdelete;
-			js_Finalize finalize;
-		} user;
-	} u;
-	js_Object *gcnext;
-	int gcmark;
-	struct js_FunctionModifier *modifiers; /* object modifiers/attributes */
+struct js_Object {
+    enum js_Class type;
+    int extensible;
+    js_Property *properties;
+    js_Property *head, **tailp; /* for enumeration */
+    int count; /* number of properties, for array sparseness check */
+    js_Object *prototype;
+    union {
+        int boolean;
+        double number;
+        struct {
+            const char *string;
+            int length;
+        } s;
+        struct {
+            int length;
+        } a;
+        struct {
+            js_Function *function;
+            js_Environment *scope;
+        } f;
+        struct {
+            const char *name;
+            js_CFunction function;
+            js_CFunction constructor;
+            int length;
+        } c;
+        js_Regexp r;
+        struct {
+            js_Object *target;
+            js_Iterator *head;
+        } iter;
+        struct {
+            const char *tag;
+            void *data;
+            js_HasProperty has;
+            js_Put put;
+            js_Delete rdelete;
+            js_Finalize finalize;
+        } user;
+    } u;
+    js_Object *gcnext;
+    int gcmark;
+    struct js_FunctionModifier *modifiers; /* object modifiers/attributes */
 };
 
-struct js_Property
-{
-	const char *name;
-	js_Property *left, *right;
-	js_Property *next, **prevp; /* for enumeration */
-	int level;
-	int atts;
-	js_Value value;
-	js_Object *getter;
-	js_Object *setter;
+struct js_Property {
+    const char *name;
+    js_Property *left, *right;
+    js_Property *next, **prevp; /* for enumeration */
+    int level;
+    int atts;
+    js_Value value;
+    js_Object *getter;
+    js_Object *setter;
+
+    js_Property *lookup(const char *name);
 };
 
-struct js_Iterator
-{
-	const char *name;
-	js_Iterator *next;
+struct js_Iterator {
+    const char *name;
+    js_Iterator *next;
 };
 
 /* jsrun.c */
@@ -168,11 +164,9 @@ double jsV_stringtonumber(js_State *J, const char *string);
 
 /* jsproperty.c */
 js_Object *jsV_newobject(js_State *J, enum js_Class type, js_Object *prototype);
-js_Property *jsV_getownproperty(js_State *J, js_Object *obj, const char *name);
 js_Property *jsV_getpropertyx(js_State *J, js_Object *obj, const char *name, int *own);
 js_Property *jsV_getproperty(js_State *J, js_Object *obj, const char *name);
 js_Property *jsV_setproperty(js_State *J, js_Object *obj, const char *name);
-js_Property *jsV_nextproperty(js_State *J, js_Object *obj, const char *name);
 void jsV_delproperty(js_State *J, js_Object *obj, const char *name);
 
 js_Object *jsV_newiterator(js_State *J, js_Object *obj, int own);
