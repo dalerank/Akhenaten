33079085b155ddf749bbe35a03af98a1f2f50587
m-anvl <mamaev-av18@ya.ru>
2026-03-23 21:56:15 +0400

    ci: refresh clang_format.sh

--- clang_format.sh
+++ clang_format.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find ./src/ -iname *.h -o -iname *.cpp -o -iname *.hpp | xargs clang-format-15 -i
+find ./src/ -iname *.h -o -iname *.cpp -o -iname *.hpp | xargs clang-format -i
