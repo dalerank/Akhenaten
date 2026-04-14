549fadd3bd65b87d7ee026593b3d38a3a56e9776
dalerank <dalerankn8@gmail.com>
2026-04-15 00:18:50 +0200

    ci: added date/time to game version string

--- src/platform/version.cpp
+++ src/platform/version.cpp
@@ -6,7 +6,8 @@
 static xstring game_version;
 xstring get_version() {
     if (game_version.empty()) {
-        game_version.printf("%u.%u.%u b%u %s", GAME_VERSION_MAJOR, GAME_VERSION_MINOR, GAME_VERSION_REVSN, GAME_BUILD_NUMBER, GAME_PLATFORM_NAME);
+        game_version.printf("%u.%u.%u b%u %s %s %s", GAME_VERSION_MAJOR, GAME_VERSION_MINOR, GAME_VERSION_REVSN,
+            GAME_BUILD_NUMBER, GAME_BUILD_DATE, GAME_BUILD_TIME, GAME_PLATFORM_NAME);
     }
     return game_version;
 }
