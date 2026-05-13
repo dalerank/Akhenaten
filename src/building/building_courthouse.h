4f5883da2c7a9d13ff3f5ab31ee6e7a21048cd4b
s.kushnirenko <sergey.kushnirenko@gaijin.team>
2026-05-13 22:37:03 +0200

    feat(courthouse): migrate spawn_figure and update_graphic to JS script
    Move courthouse roamer spawning and animation logic from C++ overrides
    into building_courthouse.js, following the building_architect_post.js pattern.
    Config params remain in buildings.js where they already exist.

--- src/building/building_courthouse.h
+++ src/building/building_courthouse.h
@@ -8,9 +8,6 @@ public:
 
     virtual building_courthouse *dcast_courthouse() override { return this; }
 
-    //virtual void on_create() override;
     virtual bool is_administration() const override { return true; }
-    virtual void spawn_figure() override;
-    virtual void update_graphic() override;
     virtual bool draw_ornaments_and_animations_height(painter &ctx, vec2i point, tile2i tile, color color_mask) override;
 };
