b211b06468240c453ab74839ae2960ec4dbd1330
dalerank <dalerankn8@gmail.com>
2026-05-13 10:33:42 +0200

    fix: implement noble figure roaming action and animation
    Nobles were spawned by noble houses but figure_action() was empty,
    so they never moved. Wire up the standard roamer state machine
    (ACTION_125_ROAMER_ROAMING / ACTION_126_ROAMER_RETURNING) and
    set the walk/death animation in update_animation().

--- src/figuretype/figure_noble.cpp
+++ src/figuretype/figure_noble.cpp
@@ -1,13 +1,32 @@
 #include "figure_noble.h"
 
+#include "figure/action.h"
 #include "js/js_game.h"
 
 REPLICATE_STATIC_PARAMS_FROM_CONFIG(figure_noble);
 
 void figure_noble::figure_action() {
+    switch (action_state()) {
+    case FIGURE_ACTION_149_CORPSE:
+        base.figure_combat_handle_corpse();
+        break;
+
+    case ACTION_125_ROAMER_ROAMING:
+        do_roam(TERRAIN_USAGE_ROADS, ACTION_126_ROAMER_RETURNING);
+        break;
+
+    case ACTION_126_ROAMER_RETURNING:
+        do_returnhome(TERRAIN_USAGE_ROADS);
+        break;
+    }
 }
 
 void figure_noble::update_animation() {
+    xstring animkey = animkeys().walk;
+    if (action_state(FIGURE_ACTION_149_CORPSE)) {
+        animkey = animkeys().death;
+    }
+    image_set_animation(animkey);
 }
 
 void figure_noble::before_poof() {
