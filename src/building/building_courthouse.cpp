40217a75e98608cd9e26bc0063d642432c4b0c2b
s.kushnirenko <sergey.kushnirenko@gaijin.team>
2026-05-13 22:37:03 +0200

    feat(courthouse): migrate spawn_figure and update_graphic to JS script
    Move courthouse roamer spawning and animation logic from C++ overrides
    into building_courthouse.js, following the building_architect_post.js pattern.
    Config params remain in buildings.js where they already exist.

--- src/building/building_courthouse.cpp
+++ src/building/building_courthouse.cpp
@@ -1,23 +1,9 @@
 #include "building/building_courthouse.h"
 
 #include "widget/city/ornaments.h"
-#include "figuretype/figure_magistrate.h"
 #include "js/js_game.h"
-#include "graphics/animation.h"
 
 REPLICATE_STATIC_PARAMS_FROM_CONFIG(building_courthouse);
-
-void building_courthouse::spawn_figure() {
-    common_spawn_roamer(FIGURE_MAGISTRATE, current_params().min_houses_coverage, (e_figure_action)ACTION_125_MAGISTRATE_ROAMING);
-}
-
-void building_courthouse::update_graphic() {
-    const xstring &animkey = can_play_animation()
-                                ? animkeys().work
-                                : animkeys().none;
-    set_animation(animkey);
-    building_impl::update_graphic();
-}
 
 bool building_courthouse::draw_ornaments_and_animations_height(painter &ctx, vec2i point, tile2i tile, color color_mask) {
     draw_normal_anim(ctx, point, tile, color_mask);
---
+++ src/scripts/building_courthouse.js
@@ -0,0 +1,14 @@
+log_info("akhenaten: building_courthouse started")
+
+[es=(building_courthouse, spawn_figure)]
+function building_courthouse_spawn_figure(ev) {
+    var building = city.get_building(ev.bid)
+    building.common_spawn_roamer(FIGURE_MAGISTRATE, building_courthouse.min_houses_coverage, ACTION_125_MAGISTRATE_ROAMING)
+}
+
+[es=(building_courthouse, update_graphic)]
+function building_courthouse_update_graphic(ev) {
+    var building = city.get_building(ev.bid)
+    var animkey = building.can_play_animation ? "work" : "none"
+    building.set_animation(animkey)
+}
