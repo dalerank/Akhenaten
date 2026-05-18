fdc91c26b6299347ca56d0325c435d0424583ad5
s.kushnirenko <sergey.kushnirenko@gaijin.team>
2026-05-18 21:48:31 +0200

    ui: auto-close overlay submenu after mouse leaves
    Restore original 500 ms submenu dismiss when the pointer leaves category
    and submenu hit areas. Track submenu_focus_time on hover; use
    submenu_close_ms on the widget. Open submenus on hover with keep_open
    cleared so pinned clicks still use keep_submenu_open.

--- src/scripts/ui_overlay_menu.js
+++ src/scripts/ui_overlay_menu.js
@@ -147,15 +147,13 @@ overlay_menu_widget {
 	selected_menu: 0
 	selected_submenu: 0
 	keep_submenu_open: 0
+	submenu_focus_time: 0
+	submenu_close_ms: 500
 	ui {
 		background    : dummy({pos[0, 0], size[155, 200]})
 		submenu_image : image({pos[-17, 6], pack:PACK_GENERAL, id:158, enabled:false})
 		submenu_item  : dummy({pos[-185, 24], size[160, 20]}) 
 	}
-}
-
-overlay_menu_widget.show = function() {
-	emit event_show_window{ id:"overlay_menu_widget" }
 }
 
 overlay_menu_widget.button_menu_item = function(index) {
@@ -182,11 +180,13 @@ overlay_menu_widget.open_submenu = function(index, keep_open) {
 	this.keep_submenu_open = keep_open
 	this.selected_menu = index
 	this.selected_submenu = index
+	this.submenu_focus_time = __game_time_millis()
 }
 
 [es=(overlay_menu_widget, init)]
 function overlay_menu_widget_init(window) {
 	overlay_menu_widget.close_submenu()
+	overlay_menu_widget.submenu_focus_time = __game_time_millis()
 }
 
 [es=(overlay_menu_widget, go_back)]
@@ -202,11 +202,14 @@ function overlay_menu_widget_ui_draw_foreground(window) {
 	__ui_window_city_draw()
 
 	var cat_size = {x:160, y:20}
-	var sub_img = window.submenu_image
 	var sub_item = window.submenu_item
 	var base_x = 0
 	var base_y = 0
 	var menus = overlay_menu.menus
+	var mx = __mouse.x - window.pos.x
+	var my = __mouse.y - window.pos.y
+	var menu_focus = false
+	var submenu_focus = false
 
 	for (var i = 0; i < menus.length; i++) {
 		var btn_pos = {x:base_x, y:base_y + i * (cat_size.y + 4)}
@@ -218,29 +221,45 @@ function overlay_menu_widget_ui_draw_foreground(window) {
 			window_go_back()
 		}
 
-		if (overlay_menu_point_in_rect(__mouse.x - window.pos.x, __mouse.y - window.pos.y, btn_pos.x, btn_pos.y, cat_size.x, cat_size.y)) {
-			overlay_menu_widget.open_submenu(i, 1)
+		if (overlay_menu_point_in_rect(mx, my, btn_pos.x, btn_pos.y, cat_size.x, cat_size.y)) {
+			menu_focus = true
+			if (menus[i].ids != undefined && menus[i].ids.length > 1) {
+				overlay_menu_widget.open_submenu(i, 0)
+			} else {
+				overlay_menu_widget.close_submenu()
+			}
 		}
 	}
 
 	if (overlay_menu_widget.selected_submenu > 0) {
 		var menu = overlay_menu.menus[overlay_menu_widget.selected_menu]
-		if (menu == undefined || menu.ids == undefined || menu.ids.length <= 1) {
-			return
-		}
+		if (menu != undefined && menu.ids != undefined && menu.ids.length > 1) {
+			for (var j = 0; j < menu.ids.length; j++) {
+				var sub_x = base_x + sub_item.pos.x
+				var sub_y = base_y + sub_item.pos.y * (j + overlay_menu_widget.selected_menu)
 
-		for (var j = 0; j < menu.ids.length; j++) {
-			var sub_x = base_x + sub_item.pos.x
-			var sub_y = base_y + sub_item.pos.y * (j + overlay_menu_widget.selected_menu)
-			
-			var sub_text = overlay_menu_overlay_title(menu.ids[j])
-			var clicked = __ui_draw_button(sub_text, [sub_x, sub_y], [sub_item.size.x, sub_item.size.y], FONT_NORMAL_BLACK_ON_DARK, UiFlags_PanelSmall, "")
-			if (clicked) {			
-				city.current_overlay = menu.ids[j]		
-				overlay_menu_widget.close_submenu()
-				window_go_back()
-				return
+				if (overlay_menu_point_in_rect(mx, my, sub_x, sub_y, sub_item.size.x, sub_item.size.y)) {
+					submenu_focus = true
+				}
+
+				var sub_text = overlay_menu_overlay_title(menu.ids[j])
+				var clicked = __ui_draw_button(sub_text, [sub_x, sub_y], [sub_item.size.x, sub_item.size.y], FONT_NORMAL_BLACK_ON_DARK, UiFlags_PanelSmall, "")
+				if (clicked) {
+					city.current_overlay = menu.ids[j]
+					overlay_menu_widget.close_submenu()
+					window_go_back()
+					return
+				}
 			}
+		}
+	}
+
+	if (!overlay_menu_widget.keep_submenu_open) {
+		if (menu_focus || submenu_focus) {
+			overlay_menu_widget.submenu_focus_time = __game_time_millis()
+		} else if (overlay_menu_widget.selected_submenu > 0
+			&& __game_time_millis() - overlay_menu_widget.submenu_focus_time > overlay_menu_widget.submenu_close_ms) {
+			overlay_menu_widget.close_submenu()
 		}
 	}
 }
