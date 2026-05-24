62ef2bb00d14d122f2ed8c447cf8a89415065eb4
s.kushnirenko <sergey.kushnirenko@gaijin.team>
2026-05-24 20:17:12 +0200

    Add integral test 07_file_dialog_load_show for window_file_dialog_load_show(FILE_TYPE_SAVED_GAME), with a __log_marker on file_dialog_load init.

---
+++ tests/07_file_dialog_load_show.js
@@ -0,0 +1,24 @@
+// Verifies that window_file_dialog_load_show(FILE_TYPE_SAVED_GAME) opens
+// file_dialog_load. The init handler logs
+// "[test-marker] window_show:file_dialog_load".
+
+function run_test() {
+    __log_info_native('[test:07] opening load savegame file dialog')
+    __test_ensure_city_session('data/default.map')
+    window_file_dialog_load_show(FILE_TYPE_SAVED_GAME)
+    __test_pump_frames(10)
+
+    window_go_back()
+    __test_pump_frames(2)
+
+    __test_signal_ready()
+}
+
+function check_valid() {
+    var marker = '[test-marker] window_show:file_dialog_load'
+    if (!__test_find_inlog(marker)) {
+        __log_info_native('[test:07] missing marker: ' + marker)
+        return false
+    }
+    return true
+}
