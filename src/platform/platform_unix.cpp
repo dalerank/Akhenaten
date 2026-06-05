b200da3a995db65b7c60275f73b60a07b02860ac
s.kushnirenko <sergey.kushnirenko@gaijin.team>
2026-06-05 23:18:08 +0200

    refactor: extract Android platform code into platform_android.cpp
    Move Android-specific SDL flags, user directory, and URL helpers out of
    platform_unix.cpp into a dedicated platform module.

--- src/platform/platform_unix.cpp
+++ src/platform/platform_unix.cpp
@@ -47,29 +47,3 @@ void platform_resolve_user_directory(bstring512& dir) {
 }
 
 #endif
-
-#if defined(GAME_PLATFORM_ANDROID)
-
-#include "core/bstring.h"
-#include "core/log.h"
-
-#include <SDL.h>
-#include <SDL_system.h>
-
-uint32_t platform_init_sdl_flags() {
-    return 0;
-}
-
-void platform_resolve_user_directory(bstring512& dir) {
-    dir = SDL_AndroidGetExternalStoragePath();
-}
-
-void platform_t::open_url(pcstr url, pcstr prefix) {
-
-}
-
-pcstr platform_t::get_steam_path() {
-    return "";
-}
-
-#endif
