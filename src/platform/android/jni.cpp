5dd94ce183e6c7f68956d915cf3996c8752d1766
s.kushnirenko <sergey.kushnirenko@gaijin.team>
2026-06-20 22:41:08 +0200

    Fix Android JNI class loading from background threads
    FindClass() could not resolve app classes such as FileManager on
    worker threads, which caused a JNI abort during sound file access.
    Load classes through the activity ClassLoader instead and clear
    pending JNI exceptions when lookup fails.

--- src/platform/android/jni.cpp
+++ src/platform/android/jni.cpp
@@ -2,6 +2,85 @@
 
 #include "SDL.h"
 
+#include <string>
+
+static void jni_clear_pending_exception(JNIEnv *env)
+{
+    if (env && env->ExceptionCheck()) {
+        env->ExceptionClear();
+    }
+}
+
+static std::string jni_slash_to_dotted(const char *class_name)
+{
+    std::string dotted(class_name ? class_name : "");
+    for (char &c : dotted) {
+        if (c == '/') {
+            c = '.';
+        }
+    }
+    return dotted;
+}
+
+static jclass jni_load_class(JNIEnv *env, jobject activity, const char *class_name)
+{
+    if (!env || !activity || !class_name) {
+        return nullptr;
+    }
+
+    jclass activity_class = env->GetObjectClass(activity);
+    if (!activity_class) {
+        jni_clear_pending_exception(env);
+        return nullptr;
+    }
+
+    jmethodID get_class_loader = env->GetMethodID(activity_class, "getClassLoader", "()Ljava/lang/ClassLoader;");
+    if (!get_class_loader) {
+        jni_clear_pending_exception(env);
+        env->DeleteLocalRef(activity_class);
+        return nullptr;
+    }
+
+    jobject class_loader = env->CallObjectMethod(activity, get_class_loader);
+    if (!class_loader) {
+        jni_clear_pending_exception(env);
+        env->DeleteLocalRef(activity_class);
+        return nullptr;
+    }
+
+    jclass class_loader_class = env->FindClass("java/lang/ClassLoader");
+    if (!class_loader_class) {
+        jni_clear_pending_exception(env);
+        env->DeleteLocalRef(class_loader);
+        env->DeleteLocalRef(activity_class);
+        return nullptr;
+    }
+
+    jmethodID load_class = env->GetMethodID(class_loader_class, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
+    if (!load_class) {
+        jni_clear_pending_exception(env);
+        env->DeleteLocalRef(class_loader_class);
+        env->DeleteLocalRef(class_loader);
+        env->DeleteLocalRef(activity_class);
+        return nullptr;
+    }
+
+    const std::string dotted = jni_slash_to_dotted(class_name);
+    jstring jclass_name = env->NewStringUTF(dotted.c_str());
+    jclass result = (jclass) env->CallObjectMethod(class_loader, load_class, jclass_name);
+
+    env->DeleteLocalRef(jclass_name);
+    env->DeleteLocalRef(class_loader_class);
+    env->DeleteLocalRef(class_loader);
+    env->DeleteLocalRef(activity_class);
+
+    if (!result) {
+        jni_clear_pending_exception(env);
+    }
+
+    return result;
+}
+
 int jni_init_function_handler(const char *class_name, jni_function_handler *handler)
 {
     handler->env = (JNIEnv *)SDL_AndroidGetJNIEnv();
@@ -14,13 +93,16 @@ int jni_init_function_handler(const char *class_name, jni_function_handler *hand
         SDL_Log("Problem loading the activity.");
         return 0;
     }
-    if(class_name) {
-        handler->nclass = handler->env->FindClass(class_name);
+    if (class_name) {
+        handler->nclass = jni_load_class(handler->env, handler->activity, class_name);
     } else {
         handler->nclass = handler->env->GetObjectClass(handler->activity);
+        if (!handler->nclass) {
+            jni_clear_pending_exception(handler->env);
+        }
     }
     if (handler->nclass == NULL) {
-        SDL_Log("Problem loading class '%s'.", class_name);
+        SDL_Log("Problem loading class '%s'.", class_name ? class_name : "<activity>");
         return 0;
     }
     return 1;
@@ -34,6 +116,7 @@ int jni_get_static_method_handler(
     }
     handler->method = handler->env->GetStaticMethodID(handler->nclass, method_name, method_signature);
     if (handler->method == NULL) {
+        jni_clear_pending_exception(handler->env);
         SDL_Log("Problem loading static method '%s' from class '%s'.", method_name, class_name);
         return 0;
     }
@@ -48,7 +131,8 @@ int jni_get_method_handler(
     }
     handler->method = handler->env->GetMethodID(handler->nclass, method_name, method_signature);
     if (handler->method == NULL) {
-        SDL_Log("Problem loading method '%s' from class '%s'.", method_name, class_name);
+        jni_clear_pending_exception(handler->env);
+        SDL_Log("Problem loading method '%s' from class '%s'.", method_name, class_name ? class_name : "<activity>");
         return 0;
     }
     return 1;
