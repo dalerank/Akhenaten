1b8bfe87128a76907780583e22f3aa631274cd9d
Mariák Kálmán <kalman.mariak@whereis.eu>
2026-07-27 20:05:51 +0200

    Add neucha-hungarian.ttf

--- src/scripts/localization.js
+++ src/scripts/localization.js
@@ -233,7 +233,7 @@ game_languages = [
     lang:"hu", caption:"Magyar", key:"hungarian",
     base_table:"localization_base_hu", table:"localization_hu", message_table:"eventmsg_hu", game_messages:"game_messages_hu"
     symbols:"A,Á,B,C,D,E,É,F,G,H,I,Í,J,K,L,M,N,O,Ó,Ö,Ő,P,Q,R,S,T,U,Ú,Ü,Ű,V,W,X,Y,Z,a,á,b,c,d,e,é,f,g,h,i,í,j,k,l,m,n,o,ó,ö,ő,p,q,r,s,t,u,ú,ü,ű,v,w,x,y,z"
-    font:"data/neucha.ttf"
+    font:"data/neucha-hungarian.ttf"
     font_configs [
       { type: FONT_SMALL_PLAIN, size:12, color:0xff000000, bold:false, line_height:12 },
       { type: FONT_NORMAL_BLACK_ON_LIGHT, size:14, color:0xff000000, bold:false, line_height:14 },
